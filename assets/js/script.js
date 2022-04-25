e72c6ea15ed9120bb9d6e104ee2c2f0f91cfaf71
elenamagay <elenamagay1985@gmail.com>
2022-04-24 23:49:22 -0400

    added one more model 'planets' and more styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 const apiUrl = 'https://swapi.dev/api/people';
 const apiUrlF = 'https://swapi.dev/api/films';
+const apiUrlP = 'https://swapi.dev/api/planets';
 let characters = [];
 let details = document.getElementById("details");
 
 document.getElementById("characters").addEventListener("click", getCharacters => {
     let tab = "";
     if (getCharacters.target.id === "characters") {
-        console.log("characters");        
+        console.log("characters button pressed");        
         fetch(apiUrl)
         .then (response => response.json())
         .then (r => r.results.forEach(element => {
@@ -22,11 +23,10 @@ document.getElementById("characters").addEventListener("click", getCharacters =>
     }
 });
 
-
 document.getElementById("films").addEventListener("click", getFilms => {
     let tab = "";
     if (getFilms.target.id === "films") {
-        console.log("films");        
+        console.log("films button pressed");        
         fetch(apiUrlF)
         .then (response => response.json())
         .then (r => r.results.forEach(element => {
@@ -36,6 +36,25 @@ document.getElementById("films").addEventListener("click", getFilms => {
                 <div>Number of the Episode: ${element.episode_id}</div>
                 <div>Director: ${element.director}</div>
                 <div>Release Date: ${element.release_date}</div>
+            </div><br/> `
+            details.innerHTML = tab;
+        }))        
+    }
+});
+
+document.getElementById("planets").addEventListener("click", getPlanets => {
+    let tab = "";
+    if (getPlanets.target.id === "planets") {
+        console.log("planets button pressed");        
+        fetch(apiUrlP)
+        .then (response => response.json())
+        .then (r => r.results.forEach(element => {
+            tab +=
+            `<div class="container">  
+                <div>Name: ${element.name}</div>
+                <div>Diameter: ${element.diameter}</div>
+                <div>Terrain: ${element.terrain}</div>
+                <div>Population: ${element.population}</div>
             </div><br/> `
             details.innerHTML = tab;
         }))        
